0db118fa9a08a3dd504425dd1adc26ef5ac983d0
MehdiH7 <mehdi.houshmand@hotmail.com>
2024-08-09 13:36:50 +0200

    Add more feature to TODO List

--- todo/script.js
+++ todo/script.js
@@ -1,63 +1,150 @@
-// IEFE
-(() => { 
-  // state variables
-  let toDoListArray = [];
-  // ui variables
+(() => {
+  // Load from localStorage or initialize empty arrays
+  let allLists = JSON.parse(localStorage.getItem('allLists')) || [];
+  let currentList = JSON.parse(localStorage.getItem('currentList')) || [];
+
   const form = document.querySelector(".form"); 
   const input = form.querySelector(".form__input");
-  const ul = document.querySelector(".toDoList"); 
+  const ul = document.querySelector(".toDoList");
+  const historyUl = document.querySelector('.historyList');
+  const createNewListBtn = document.querySelector('.create-new-list');
+  const deleteHistoryBtn = document.querySelector('.delete-history');
+  const hamburgerBtn = document.querySelector('.hamburger');
+  const sidebar = document.querySelector('.sidebar');
+  const closeBtn = document.querySelector('.close-btn');
 
-  // event listeners
+  // Load existing to-do list items
+  loadToDoList();
+
+  // Load history in sidebar
+  loadHistory();
+
   form.addEventListener('submit', e => {
-    // prevent default behaviour - Page reload
     e.preventDefault();
-    // give item a unique ID
     let itemId = String(Date.now());
-    // get/assign input value
     let toDoItem = input.value;
-    //pass ID and item into functions
-    addItemToDOM(itemId , toDoItem);
-    addItemToArray(itemId, toDoItem);
-    // clear the input box. (this is default behaviour but we got rid of that)
+    let date = new Date().toLocaleDateString();
+
+    addItemToDOM(itemId, toDoItem);
+    addItemToArray(itemId, toDoItem, date);
+    saveCurrentListToLocalStorage();
     input.value = '';
   });
-  
+
   ul.addEventListener('click', e => {
-    let id = e.target.getAttribute('data-id')
-    if (!id) return // user clicked in something else      
-    //pass id through to functions
-    removeItemFromDOM(id);
-    removeItemFromArray(id);
+    let id = e.target.getAttribute('data-id');
+    if (!id) return;
+    toggleItemDone(id);
+    saveCurrentListToLocalStorage();
   });
-  
-  // functions 
-  function addItemToDOM(itemId, toDoItem) {    
-    // create an li
-    const li = document.createElement('li')
+
+  createNewListBtn.addEventListener('click', () => {
+    let date = new Date().toLocaleDateString();
+
+    // Save the current list before clearing it
+    if (currentList.length > 0) {
+      saveCurrentListWithDate(date);
+      clearCurrentList();
+      loadHistory();
+    }
+  });
+
+  deleteHistoryBtn.addEventListener('click', () => {
+    // Clear all data in localStorage
+    localStorage.removeItem('allLists');
+    localStorage.removeItem('currentList');
+    allLists = [];
+    clearCurrentList();
+    loadHistory();
+  });
+
+  historyUl.addEventListener('click', e => {
+    let listIndex = e.target.getAttribute('data-index');
+    if (listIndex === null) return;
+    loadSpecificList(listIndex);
+    closeSidebar();
+  });
+
+  hamburgerBtn.addEventListener('click', openSidebar);
+  closeBtn.addEventListener('click', closeSidebar);
+
+  function addItemToDOM(itemId, toDoItem, done = false) {
+    const li = document.createElement('li');
     li.setAttribute("data-id", itemId);
-    // add toDoItem text to li
-    li.innerText = toDoItem
-    // add li to the DOM
+    li.innerText = toDoItem;
+    if (done) {
+      li.classList.add('done');
+    }
     ul.appendChild(li);
   }
-  
-  function addItemToArray(itemId, toDoItem) {
-    // add item to array as an object with an ID so we can find and delete it later
-    toDoListArray.push({ itemId, toDoItem});
-    console.log(toDoListArray)
+
+  function addItemToArray(itemId, toDoItem, date, done = false) {
+    currentList.push({ itemId, toDoItem, date, done });
   }
-  
-  function removeItemFromDOM(id) {
-    // get the list item by data ID
-    var li = document.querySelector('[data-id="' + id + '"]');
-    // remove list item
-    ul.removeChild(li);
+
+  function toggleItemDone(id) {
+    const li = document.querySelector('[data-id="' + id + '"]');
+    li.classList.toggle('done');
+    const item = currentList.find(item => item.itemId === id);
+    if (item) {
+      item.done = !item.done;
+    }
   }
-  
-  function removeItemFromArray(id) {
-    // create a new toDoListArray with all li's that don't match the ID
-    toDoListArray = toDoListArray.filter(item => item.itemId !== id);
-    console.log(toDoListArray);
+
+  function saveCurrentListToLocalStorage() {
+    localStorage.setItem('currentList', JSON.stringify(currentList));
   }
-  
+
+  function saveAllListsToLocalStorage() {
+    localStorage.setItem('allLists', JSON.stringify(allLists));
+  }
+
+  function clearCurrentList() {
+    currentList = [];
+    ul.innerHTML = '';
+    localStorage.removeItem('currentList');
+  }
+
+  function saveCurrentListWithDate(date) {
+    // Find how many lists exist for today
+    const existingEntriesForDate = allLists.filter(item => item.listId.startsWith(date));
+    const count = existingEntriesForDate.length + 1;
+
+    // Create a unique identifier for the list
+    const uniqueListIdentifier = `${date} (List ${count})`;
+
+    // Save the list with this identifier
+    allLists.push({ listId: uniqueListIdentifier, items: [...currentList] });
+    saveAllListsToLocalStorage();
+  }
+
+  function loadToDoList() {
+    ul.innerHTML = '';
+    currentList.forEach(item => addItemToDOM(item.itemId, item.toDoItem, item.done));
+  }
+
+  function loadSpecificList(listIndex) {
+    ul.innerHTML = '';
+    currentList = [...allLists[listIndex].items];
+    saveCurrentListToLocalStorage();
+    loadToDoList();
+  }
+
+  function loadHistory() {
+    historyUl.innerHTML = '';
+    allLists.forEach((list, index) => {
+      const li = document.createElement('li');
+      li.setAttribute("data-index", index);
+      li.innerText = list.listId;
+      historyUl.appendChild(li);
+    });
+  }
+
+  function openSidebar() {
+    sidebar.classList.add('open');
+  }
+
+  function closeSidebar() {
+    sidebar.classList.remove('open');
+  }
 })();
